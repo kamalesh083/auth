f50b28d83bae6c65febb26efe7c3cdb08071206d
kamalesh083 <kamalesh083@gmail.com>
2025-01-23 19:05:45 +0530

    login update

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -82,7 +82,33 @@ export const verifyEmail = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("login route");
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+    const isPassword = await bcrypt.compare(password, user.password);
+    if (!isPassword) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+    generateTokenAndSetCookie(res, user._id);
+    user.lastLogin = new Date();
+    await user.save();
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: "Logged in successfully",
+        user: { ...user._doc, password: undefined },
+      });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
 
 export const logout = async (req, res) => {
